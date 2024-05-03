8790b49bb02651800f07c233a2d58b2ce055cf36
Your Name <you@example.com>
2024-05-03 12:41:36 -0700

    First commit

--- projet/settings.py
+++ projet/settings.py
@@ -78,15 +78,15 @@ WSGI_APPLICATION = 'projet.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-DATABASE={
-    'default':dj_database_url.config()
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+# DATABASE={
+#     'default':dj_database_url.config()
+# }
 
 
 # Password validation
